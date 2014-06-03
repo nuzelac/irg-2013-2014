09c971abd7c0efd4b52a131e151e75c09eac1097
Nino Uzelac <uzelac.nino@gmail.com>
2014-06-03 15:06:43 +0200

    fajlovi za vjezbu7

---
+++ IRG/vjezba7/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  vjezba7
+//
+//  Created by Nino Uzelac on 03/06/14.
+//  Copyright (c) 2014 Nino Uzelac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
